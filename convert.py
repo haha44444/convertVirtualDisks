c906b2f3ef53334cb6c78b0b367c83112d743200
haha44444 <jk077088@outlook.com>
2023-11-16 09:16:49 +0800

    add del file after converted

--- convert.py
+++ convert.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
+import codecs
 import os
+import sys
+
+sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
 
 def start():
-    global input_disk_type, output_disk_type
+    global input_disk_type, output_disk_type, del_input_file
     # 创建文件夹
     if not os.path.exists("./convertDisks"):
         os.mkdir("convertDisks")
@@ -27,6 +31,13 @@ def start():
     print("输入转换后的文件类型，必须为以下类型中的一种")
     print("vhd vmdk qcow2 raw vhdx qcow vdi qed")
     output_disk_type = input()
+    print("转换后是否删除转换前的文件")
+    print("是:0   否:1")
+    del_input_file_num = input()
+    if del_input_file_num == 0:
+        del_input_file = True
+    else:
+        del_input_file = False
 
 
 def convert_disks_file_name_list():
@@ -41,7 +52,8 @@ def convert(disk_file_name):
     # 不完整命令
     path1 = convert_file_path + "/"
     path2 = "./convertedDisks/"
-    cmd = "qemu-img convert -p -f {0} -O {1} {3}{2}.{0} {4}{2}.{1}".format(input_disk_type, output_disk_type, disk_file_name, path1, path2)
+    cmd = "qemu-img convert -p -f {0} -O {1} {3}{2}.{0} {4}{2}.{1}".format(input_disk_type, output_disk_type,
+                                                                           disk_file_name, path1, path2)
     return cmd
 
 
@@ -54,9 +66,15 @@ def main():
         convert_cmd = convert(file_name_without_suffix)
         # 开始转换
         os.system(convert_cmd)
+        # 删除转换前文件
+        if del_input_file == True:
+            os.remove(convert_file_path + "/" + file_name_without_suffix + "." + input_disk_type)
+            print("已删除转换前的文件")
+        else:
+            pass
+        print("转换完成，转换后的磁盘文件输出到convertedDisks文件夹中")
 
 
 if __name__ == '__main__':
     start()
     main()
-    print("转换完成，转换后的磁盘文件输出到convertedDisks文件夹中")
